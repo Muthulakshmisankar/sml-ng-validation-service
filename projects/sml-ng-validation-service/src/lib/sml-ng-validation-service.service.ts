a4aae830284d567fa37f1c64e9555b54ebd8044a
lakshmi <muthulakshmistvl@gmail.com>
2021-03-15 19:10:43 +0530

    validation changes

--- projects/sml-ng-validation-service/src/lib/sml-ng-validation-service.service.ts
+++ projects/sml-ng-validation-service/src/lib/sml-ng-validation-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, HostListener } from '@angular/core';
 import { ValidatorFn, AbstractControl, FormControl } from '@angular/forms';
 @Injectable({
   providedIn: 'root'
@@ -30,32 +30,30 @@ export class SmlNgValidationService {
   public allowOnly(Regex, e) {
     this.preventCharacters(this.regexMapping[Regex], e);
   }
+  public notAllow(Regex, e) {
+    this.notAllowCharacters(this.regexMapping[Regex], e);
+  }
   public userDefinedRegex(e, FirstChar, LastChar, IgnoreCaseSensitive?: boolean) {
-    //Ranges & limits
-    const input = String.fromCharCode(e.charCode);
-    const alpha = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz'
-    let regexp: any = /[a-z]/;
-    if (IgnoreCaseSensitive) {
-      regexp = /[a-z]/i;
-    }
-    const Alpha = alpha.match(regexp);
-    const indexFirstChar = Alpha.indexOf(FirstChar);
-    const indexLastChar = Alpha.indexOf(LastChar);
-    const indexInputChar = Alpha.indexOf(input);
-    if (indexFirstChar > indexInputChar || indexInputChar > indexLastChar) {
-      e.preventDefault();
-    }
+    const pattern = '[' + FirstChar + '-' + LastChar + ']';
+    const RegexPattern = IgnoreCaseSensitive ? new RegExp(pattern, 'i') : new RegExp(pattern);
+    this.preventCharacters(RegexPattern, e);
+
   }
   public preventCharacters(pattern, event) {
-    const input = String.fromCharCode(event.charCode) || event.clipboardData.getData('Text');
+    const input = (event.type === 'paste') ? event.clipboardData.getData('Text') : String.fromCharCode(event.charCode);
     if (!pattern.test(input)) {
       event.preventDefault();
     }
   }
+  public notAllowCharacters(pattern, event) {
 
+    const input = (event.type === 'paste') ? event.clipboardData.getData('Text') : String.fromCharCode(event.charCode);
+    if (pattern.test(input)) {
+      event.preventDefault();
+    }
+  }
   public userDefinedPattern(event, CharacterClass1, CharacterClass2, IgnoreCaseSensitive?: boolean, array?: Array<any>) {
     //CharacterClasses
-
     let Char = this.regexConstructor[CharacterClass1];
     let Char2 = this.regexConstructor[CharacterClass2];
     if (Char2) {
@@ -77,25 +75,38 @@ export class SmlNgValidationService {
   public EmailId(): ValidatorFn {
     return (control: AbstractControl): any => {
       if (control.value !== undefined) {
-        const emailpattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
         const emailIdPattern = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,10})$/;
         if (!emailIdPattern.test(control.value)) {
-          return (control.value) ? null : { 'SetError': { valid: false } };
+          return { 'isEmailValid': { valid: false } };
+        } else {
+          return null;
         }
       }
       return null;
     };
     // const pattern = /[^\d{1}$]/; // not understand
     // const patterns = /\b[1-5]/;  // between
-   
+
     // const regex = new RegExp(email);
     // this.preventCharacters(regex, event);
   }
-
-  public MinMax(controlcode, minVal, maxVal): ValidatorFn {
+  public PhoneNumber(): ValidatorFn {
     return (control: AbstractControl): any => {
       if (control.value !== undefined) {
-        if (controlcode === 'NumberOnly') {
+        const phoneNumberPattern = /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/;
+        if (!phoneNumberPattern.test(control.value)) {
+          return { 'isPhoneNumberValid': { valid: false } };
+        } else {
+          return null;
+        }
+      }
+      return null;
+    };
+  }
+  public MinMax(controlDataType, minVal, maxVal): ValidatorFn {
+    return (control: AbstractControl): any => {
+      if (control.value !== undefined) {
+        if (controlDataType === 'Integer') {
           const pattern = /[0-9]/
           for (let i = 0; i < control.value.length; i++) {
             const input = control.value.substring(i, i + 1)
@@ -106,13 +117,112 @@ export class SmlNgValidationService {
           if (control.value.length < parseInt(minVal) || control.value.length > parseInt(maxVal)) {
             if (parseInt(minVal) === parseInt(maxVal)) {
               return { 'MinMax': { valid: 'Length should be ' + minVal } };
-            }
-            else {
-              return { 'MinMax': { valid: 'Length should between' + minVal + ' and ' + minVal } };
+            } else {
+              return { 'MinMax': { valid: 'Length should between ' + minVal + ' and ' + maxVal } };
             }
           }
-        } 
+        } else if (controlDataType === 'Float') {
+          const pattern = /[0-9]/
+          for (let i = 0; i < control.value.length; i++) {
+            const input = control.value.substring(i, i + 1)
+            if (!pattern.test(input)) {
+              return { 'MinMax': { valid: 'Only Numbers Must Be Allowed' } }
+            }
+          }
+          if (control.value.length < parseFloat(minVal) || control.value.length > parseFloat(maxVal)) {
+            if (parseFloat(minVal) === parseFloat(maxVal)) {
+              return { 'MinMax': { valid: 'Length should be ' + minVal } };
+            } else {
+              return { 'MinMax': { valid: 'Length should between ' + minVal + ' and ' + maxVal } };
+            }
+          }
+        } else if (controlDataType === 'String') {
+          const pattern = /[a-zA-Z]/
+          for (let i = 0; i < control.value.length; i++) {
+            const input = control.value.substring(i, i + 1)
+            if (!pattern.test(input)) {
+              return { 'MinMax': { valid: 'Only string Must Be Allowed' } }
+            }
+          }
+          if (control.value.length < parseInt(minVal) || control.value.length > parseInt(maxVal)) {
+            if (parseInt(minVal) === parseInt(maxVal)) {
+              return { 'MinMax': { valid: 'Length should be ' + minVal } };
+            } else {
+              return { 'MinMax': { valid: 'Length should between ' + minVal + ' and ' + maxVal } };
+            }
+          }
+        }
       }
+    }
+  }
+  public MaskPhoneNumber(event, eventval?: any) {
+    if (event) {
+      let eventValue = event.target.value;
+      let newVal = eventValue.replace(/\D/g, '');
+      if (newVal.length === 0) {
+        newVal = '';
+      } else if (newVal.length <= 2) {
+        newVal = newVal.replace(/^(\d{0,3})/, '$1');
+      } else if (newVal.length <= 5) {
+        newVal = newVal.replace(/^(\d{0,3})(\d{0,3})/, '$1-$2');
+      } else if (newVal.length <= 9) {
+        newVal = newVal.replace(/^(\d{0,3})(\d{0,3})(\d{0,4})/, '$1-$2-$3');
+      } else {
+        event.preventDefault();
+        return;
+      }
+      event.target.value = newVal
+      console.log('Value', newVal);
+    }
+  }
+  public sliceNthElementsFromArray(arrayItems, nThItemsTobeSlice, noOfItemsToSlice) {
+    let nthposition = parseInt(nThItemsTobeSlice) + 1;
+    let total_slice_count = Math.ceil(arrayItems.length / noOfItemsToSlice);
+    if (nThItemsTobeSlice == 0) {
+      return { Error: 'Please provide valid position' };
+    }
+    if (total_slice_count < nThItemsTobeSlice) {
+      return { Error: 'Please provide valid position' };
+    }
+    let offSetItems = (parseInt(nThItemsTobeSlice) - 1) * noOfItemsToSlice;
+    let slicedItems = arrayItems.slice(offSetItems).slice(0, noOfItemsToSlice);
+    let lastPosition = (total_slice_count - parseInt(nThItemsTobeSlice) === 0) ? true : false;
+    if (lastPosition) {
+      let pendingElementsCount = arrayItems.length - (parseInt(nThItemsTobeSlice) * noOfItemsToSlice);
+      if (pendingElementsCount > 0) {
+        slicedItems = arrayItems.slice(Math.max(arrayItems.length - pendingElementsCount, 0));
+      }
+    }
+    let slicedItem = {
+      offSetItems: offSetItems,
+      slicedItems: slicedItems,
+      lastPosition: lastPosition,
+      pendingElementsIndex: arrayItems.length - (nthposition * noOfItemsToSlice),
+      nThItemsTobeSlice: nThItemsTobeSlice,
+      noOfItemsToSlice: noOfItemsToSlice,
+      nthposition: nthposition
+    }
+    return slicedItem;
+  }
+  public notAllowedMaxLength(event, minVal, maxVal, whichevent = '') {
+    let input = ''
+    if (whichevent === 'paste') {
+      const clipData = event.clipboardData;
+      input = clipData.getData('text');
+    } else {
+      input = event.target.value;
+    }
+    if (input.length === parseInt(maxVal)) {
+      if (event.target.selectionStart < parseInt(maxVal) && input.length <= parseInt(maxVal)) {
+        // event.target.value = event.target.value.substring(0,event.target.selectionStart );
+        if (input.length > parseInt(maxVal)) {
+          event.preventDefault();
+        }
+      } else {
+        event.preventDefault();
+      }
+    } else if (whichevent === 'paste' && (input.length < parseInt(minVal) || input.length > parseInt(maxVal))) {
+      event.preventDefault();
     }
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,19 +9,42 @@ import { SmlNgValidationService } from 'projects/sml-ng-validation-service/src/p
 export class AppComponent implements OnInit {
   title = 'sml-validation-service';
   smlNgValidationForm: FormGroup;
+  slicedArray: Array<any> = [];
+  selectedRegex: any;
+  regexPatternsArray: Array<any> = [
+    { 'value': 'Alpha', 'label': 'Enter Alphabet only' },
+    { 'value': 'Number', 'label': 'Enter Number only' },
+    { 'value': 'SpecialChar', 'label': 'Enter special character only' },
+    { 'value': 'anythingExceptSpace', 'label': 'Enter any characters except space' },
+    { 'value': 'anythingExceptNumber', 'label': 'Enter anything except number' },
+    { 'value': 'anythingExceptSpecialChar', 'label': 'Enter anything except special character' },
+    { 'value': 'AlphaWithSpace', 'label': 'Enter Alphabet with space' },
+    { 'value': 'onlySpace', 'label': 'Enter space only' }]
   constructor(
     private fb: FormBuilder,
     public validation: SmlNgValidationService
   ) { }
-  get smlNgValidationFormControls():any{
+  get smlNgValidationFormControls(): any {
     return this.smlNgValidationForm.controls;
   }
-  ngOnInit(){
+  ngOnInit() {
     this.smlNgValidationForm = this.fb.group({
-      name: ['',Validators.required],
-      telephone: ['',Validators.required],
-      email: ['',Validators.required],
-      address: ['',Validators.required]
+      stringWithSpace: ['', Validators.required],
+      regexDefined: ['', [Validators.required]],
+      StrregexDefined: ['', [Validators.required]],
+      lengthDefined: ['', [Validators.required, this.validation.MinMax('Integer', 2, 2)]],
+      email: ['', [Validators.required, this.validation.EmailId()]],
+      telephone: ['', [Validators.required, this.validation.PhoneNumber()]],
+      sliceNthElement: ['']
     });
+  }
+  sliceArrayItems(event) {
+    let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17]
+    let slicedObj: any = this.validation.sliceNthElementsFromArray(arr, this.smlNgValidationFormControls.sliceNthElement.value, '3');
+    console.log('returned', slicedObj)
+    this.slicedArray = slicedObj.Error ? slicedObj.Error : slicedObj.slicedItems;
+  }
+  setRegex() {
+   this.smlNgValidationFormControls.stringWithSpace.setValue('');
   }
 }
